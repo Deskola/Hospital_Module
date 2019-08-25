1ffb8b2516df6100302a96b06d99fc6931903adc
Deskola <dennis.ogunde@strathmore.edu>
2019-08-26 00:23:40 +0300

    comlplete nurse interface

--- resources/views/admin/pages/Info/create.blade.php
+++ resources/views/admin/pages/Info/create.blade.php
@@ -109,59 +109,66 @@
                                 </div>
                             </div>
                             <div class="form-group">
-                                <div class="col-sm-9">                                    
-                                    <label for="Date_of_Birth" class=" control-label">Mental health conditionsh and Substance Abuse</label>
-                                    <input type="text" class="form-control" name="mental_health_condition" id="mental_health_condition" placeholder="Mental health conditionsh and Substance Abuse" >                            
-                                </div>
-                            </div>                            
-                        </div>
-                        <div class="col-lg-6">
-                        <div class="form-group">
                             <div class="col-sm-9">                                    
                                 <label for="Date_of_Birth" class=" control-label">Mental health conditionsh and Substance Abuse</label>
-                                <input type="text" class="form-control" name="mental_health_condition" id="mental_health_condition" placeholder="Mental health conditionsh and Substance Abuse" >                            
+                                <textarea class="form-control"name="mental_health_condition" rows="5" >{{old('')}}</textarea>                            
                             </div>
-                            </div>  
+                            </div>                          
+                        </div>
+                        <div class="col-lg-6">
+                         
                         <div class="form-group">
                             <div class="col-sm-9">                                    
                                 <label for="Date_of_Birth" class=" control-label">Pregnancy complications</label>
-                                <input type="text" class="form-control" name="pregnancy_complications" id="pregnancy_complications" placeholder="Mental health conditionsh and Substance Abuse" >                            
+                                <textarea class="form-control"name="pregnancy_complication" rows="5" >{{old('')}}</textarea>                           
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-9">                                    
+                                <label for="Date_of_Birth" class=" control-label">Deceased relatives cause of death</label>
+                                <textarea class="form-control"name="cause_of_death" rows="5" >{{old('')}}</textarea>                           
                             </div>
                         </div>
                             
-                        </div></div><hr> 
+                        </div></div>  
+                        <hr> 
 
                 <p class="text-info">Medical History Information</p>
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="form-group">
                                 <div class="col-md-9">
-                                    <label for="Date_of_Birth" class=" control-label">Pick date of Birth</label>
-                                    <input type="date" class="form-control" name="Date_of_Birth" placeholder="Date Of birth" value="{{old('Date_of_Birth')}}" required>
+                                    <label for="Date_of_Birth" class=" control-label">Weight</label>
+                                    <input type="number" class="form-control" name="weight" placeholder="weight"  required>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <div class="col-sm-9">
-                                    <label for="religion" class="control-label">National Identification Number</label>
-                                    <input type="number" class="form-control" name="national_id" id="national_id" placeholder="Natioanl ID/Huduma Number">                                    
+                                    <label for="religion" class="control-label">Height</label>
+                                    <input type="number" class="form-control" name="height" id="height" placeholder="height" required>                                    
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="form-group">
                                 <div class="col-sm-9">
-                                    <label for="school_attended" class=" control-label">Residential Area</label>
-                                    <input type="text" name="residential_area" class="form-control" id="residential_area" placeholder="Residential Area" required>
+                                    <label for="school_attended" class=" control-label">Blood Pessure</label>
+                                    <input type="text" name="blood_pressure" class="form-control" id="blood_pressure" placeholder="Blood Pressure" required>
                                 </div>
                             </div>
 
                              <div class="form-group">
                                 <div class="col-sm-9">
-                                    <label class=" control-label">Email Address</label>
-                                    <input type="email" class="form-control" name="email_address" id="email_address" placeholder="Email address" required>
+                                    <label class=" control-label">Temperature</label>
+                                    <input type="number" class="form-control" name="temperature" id="temperature" placeholder="Temperature" required>
                                 </div>
                             </div>
-                        </div></div><hr>                      
+                        </div></div>
+                        <div class="form-group">
+                            <label>Description of the reason for current visit</label>
+                            <textarea class="form-control"name="medical_info" rows="5" required></textarea>
+                        </div><hr> 
+                                            
 
                       <div class="form-group">
                           <button  type="submit"  class="btn btn-primary mb-2">Submit Details</button>
